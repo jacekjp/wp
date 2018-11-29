d67197c036ce7dd1cb430e3a9fb79c7cb4080ebf
jacekjp <jacekjp@gmail.com>
2018-11-29 21:49:37 +0100

    add index.php

---
+++ themes/jjpbasic/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+    <meta charset="<?php bloginfo('charset'); ?>">
+    <title><?php bloginfo('name'); ?></title>
+    <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
+    <?php wp_head(); ?>
+</head>
+<body>
+    <header>
+        <h1><?php bloginfo('name'); ?></h1>
+        <span><?php bloginfo('description'); ?></span>
+    </header>
+</body>
+</html>
