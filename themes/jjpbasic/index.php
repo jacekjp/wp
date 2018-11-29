6f6bb2ce0ae76e9af347ba5656534e7098986f21
jacekjp <jacekjp@gmail.com>
2018-11-29 22:57:47 +0100

    style posts list

--- themes/jjpbasic/index.php
+++ themes/jjpbasic/index.php
@@ -4,15 +4,17 @@
     <div class="container">
         <?php if (have_posts()) : ?>
             <?php while (have_posts()): the_post(); ?>
-                <h3>
-                    <a href="<?php the_permalink(); ?>">
-                        <?php the_title(); ?>
-                    </a>
-                </h3>
-                <div class="meta">
-                    Created By <?php the_author(); ?> on <?php the_time(get_option('date_format')); ?>
-                </div>
-                <?php the_content(); ?>
+                <article class="post">
+                    <h3>
+                        <a href="<?php the_permalink(); ?>">
+                            <?php the_title(); ?>
+                        </a>
+                    </h3>
+                    <div class="meta">
+                        Created By <?php the_author(); ?> on <?php the_time(get_option('date_format')); ?>
+                    </div>
+                    <?php the_content(); ?>
+                </article>
             <?php endwhile; ?>
         <?php else : ?>
             <?php echo wpautop('No posts ware found'); ?>
