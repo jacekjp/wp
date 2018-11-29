cee7eaeeb620861679421745b6c01528bc1ae85a
jacekjp <jacekjp@gmail.com>
2018-11-29 23:35:27 +0100

    use excerpts in posts list

--- themes/jjpbasic/functions.php
+++ themes/jjpbasic/functions.php
@@ -6,3 +6,10 @@ function basic_theme_setup(){
 }
 
 add_action('after_setup_theme', 'basic_theme_setup');
+
+//Excerpt Length
+function set_excerpt_length(){
+    return 50;
+}
+
+add_filter('excerpt_length', 'set_excerpt_length');
