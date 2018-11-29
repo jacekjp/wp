6c8cb0298a75faadc10dc8f097c41646905b42cb
jacekjp <jacekjp@gmail.com>
2018-11-29 22:53:18 +0100

    add permalink to post titles

--- themes/jjpbasic/index.php
+++ themes/jjpbasic/index.php
@@ -4,7 +4,11 @@
     <div class="container">
         <?php if (have_posts()) : ?>
             <?php while (have_posts()): the_post(); ?>
-                <h3><?php the_title(); ?></h3>
+                <h3>
+                    <a href="<?php the_permalink(); ?>">
+                        <?php the_title(); ?>
+                    </a>
+                </h3>
                 <div class="meta">
                     Created By <?php the_author(); ?> on <?php the_time(get_option('date_format')); ?>
                 </div>
