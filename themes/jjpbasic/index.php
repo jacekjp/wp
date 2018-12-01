51161bab495f5199efb1404dee112b11abe9d67b
jacekjp <jacekjp@gmail.com>
2018-12-01 13:46:36 +0100

    show widgets in sidebar

--- themes/jjpbasic/index.php
+++ themes/jjpbasic/index.php
@@ -31,7 +31,9 @@
 
     </div>
     <div class="sidebar">
-        TEST
+        <?php if(is_active_sidebar('sidebar')) : ?>
+            <?php dynamic_sidebar('sidebar'); ?>
+        <?php endif; ?>
     </div>
     <div class="clr"></div>
 </div>
