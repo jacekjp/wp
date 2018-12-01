8f7befdc335b88d11a1ae7275467379cfd423c4b
jacekjp <jacekjp@gmail.com>
2018-12-01 16:57:17 +0100

    initialize theme with html template

---
+++ themes/jjpadvanced/index.php
@@ -0,0 +1,133 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Advanced Theme</title>
+    <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>"/>
+</head>
+<body>
+<header>
+    <div class="container">
+        <h1>
+            <a href="index.html">Advanced Theme</a>
+            <small>Another WordPress Theme</small>
+        </h1>
+
+        <div class="h_right">
+            <form action="">
+                <input type="text" placeholder="Search..."/>
+            </form>
+        </div>
+    </div>
+</header>
+
+<nav class="nav main-nav">
+    <div class="container">
+        <ul>
+            <li><a href="index.html">Home</a></li>
+            <li><a href="about.html">About</a></li>
+            <li><a href="#">Services</a></li>
+        </ul>
+    </div>
+</nav>
+
+<div class="container content">
+    <div class="main block">
+        <article class="post">
+            <h2>Blog Post 1</h2>
+
+            <p class="meta">Posted at 15:44 on December 1 by admin</p>
+
+            <p>
+                Lorizzle ipsizzle get down get down sizzle amizzle, shit adipiscing that's the shizzle. Nullizzle dizzle
+                velizzle, aliquet volutpizzle, suscipit quis, da bomb vel, funky fresh. Things mammasay mammasa mamma oo
+                sa tortizzle. Sure boofron. Fusce funky fresh dolizzle dapibizzle bizzle tempizzle pizzle. Mauris
+                pellentesque nibh shiznit turpizzle. We gonna chung da bomb for sure. Pellentesque eleifend rhoncizzle
+                nisi. In hac habitasse platea gangsta. Shiz dapibus. Curabitur get down get down urna, pretizzle check
+                it out, mattizzle the bizzle, eleifend vitae, nunc. Away izzle. Shiznit fo shizzle velit cool for sure.
+            </p>
+
+            <p>
+                Etizzle fo shizzle funky fresh sed augue hendrerit accumsizzle. I'm in the shizzle izzle fo shizzle.
+                Vivamizzle rizzle dolizzle, viverra vitae, sizzle izzle, ultrices izzle, sizzle. Vestibulum ante ipsum
+                ass izzle my shizz orci crunk izzle ultrices posuere cubilia Curae; Donizzle dolizzle. Crazy faucibus.
+                Maecenas pharetra blandit doggy. Gangster rutrum aliquizzle orci. Gangster facilisizzle. You son of a
+                bizzle daahng dawg , venenatis phat, scelerisque sheezy, blandizzle izzle, magna.
+            </p>
+            <a href="#" class="button">Read More</a>
+        </article>
+        <article class="post">
+            <h2>Blog Post 2</h2>
+
+            <p class="meta">Posted at 15:44 on December 1 by admin</p>
+
+            <p>
+                Lorizzle ipsizzle get down get down sizzle amizzle, shit adipiscing that's the shizzle. Nullizzle dizzle
+                velizzle, aliquet volutpizzle, suscipit quis, da bomb vel, funky fresh. Things mammasay mammasa mamma oo
+                sa tortizzle. Sure boofron. Fusce funky fresh dolizzle dapibizzle bizzle tempizzle pizzle. Mauris
+                pellentesque nibh shiznit turpizzle. We gonna chung da bomb for sure. Pellentesque eleifend rhoncizzle
+                nisi. In hac habitasse platea gangsta. Shiz dapibus. Curabitur get down get down urna, pretizzle check
+                it out, mattizzle the bizzle, eleifend vitae, nunc. Away izzle. Shiznit fo shizzle velit cool for sure.
+            </p>
+
+            <p>
+                Etizzle fo shizzle funky fresh sed augue hendrerit accumsizzle. I'm in the shizzle izzle fo shizzle.
+                Vivamizzle rizzle dolizzle, viverra vitae, sizzle izzle, ultrices izzle, sizzle. Vestibulum ante ipsum
+                ass izzle my shizz orci crunk izzle ultrices posuere cubilia Curae; Donizzle dolizzle. Crazy faucibus.
+                Maecenas pharetra blandit doggy. Gangster rutrum aliquizzle orci. Gangster facilisizzle. You son of a
+                bizzle daahng dawg , venenatis phat, scelerisque sheezy, blandizzle izzle, magna.
+            </p>
+            <a href="#" class="button">Read More</a>
+        </article>
+        <article class="post">
+            <h2>Blog Post 3</h2>
+
+            <p class="meta">Posted at 15:44 on December 1 by admin</p>
+
+            <p>
+                Lorizzle ipsizzle get down get down sizzle amizzle, shit adipiscing that's the shizzle. Nullizzle dizzle
+                velizzle, aliquet volutpizzle, suscipit quis, da bomb vel, funky fresh. Things mammasay mammasa mamma oo
+                sa tortizzle. Sure boofron. Fusce funky fresh dolizzle dapibizzle bizzle tempizzle pizzle. Mauris
+                pellentesque nibh shiznit turpizzle. We gonna chung da bomb for sure. Pellentesque eleifend rhoncizzle
+                nisi. In hac habitasse platea gangsta. Shiz dapibus. Curabitur get down get down urna, pretizzle check
+                it out, mattizzle the bizzle, eleifend vitae, nunc. Away izzle. Shiznit fo shizzle velit cool for sure.
+            </p>
+
+            <p>
+                Etizzle fo shizzle funky fresh sed augue hendrerit accumsizzle. I'm in the shizzle izzle fo shizzle.
+                Vivamizzle rizzle dolizzle, viverra vitae, sizzle izzle, ultrices izzle, sizzle. Vestibulum ante ipsum
+                ass izzle my shizz orci crunk izzle ultrices posuere cubilia Curae; Donizzle dolizzle. Crazy faucibus.
+                Maecenas pharetra blandit doggy. Gangster rutrum aliquizzle orci. Gangster facilisizzle. You son of a
+                bizzle daahng dawg , venenatis phat, scelerisque sheezy, blandizzle izzle, magna.
+            </p>
+            <a href="#" class="button">Read More</a>
+        </article>
+    </div>
+    <div class="side">
+        <div class="block">
+            <h3>Sidebar Head</h3>
+
+            <p>Etizzle fo shizzle funky fresh sed augue hendrerit accumsizzle. I'm in the shizzle izzle fo shizzle.
+                Vivamizzle rizzle dolizzle, viverra vitae, sizzle izzle, ultrices izzle, sizzle.</p>
+            <a href="" class="button">More</a>
+        </div>
+    </div>
+</div>
+<footer class="main-footer">
+    <div class="container">
+        <div class="f_left">
+            <p>&copy; 2018 - JJP</p>
+        </div>
+        <div class="f_right">
+            <ul>
+                <li><a href="index.html">Home</a></li>
+                <li><a href="about.html">About</a></li>
+                <li><a href="#">Services</a></li>
+            </ul>
+        </div>
+    </div>
+</footer>
+</body>
+</html>
+
+
+
