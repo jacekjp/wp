e37387992a480f2a41694a5df23c03cb26129ad8
jacekjp <jacekjp@gmail.com>
2018-12-01 17:45:13 +0100

    add categories to post meta

--- themes/jjpadvanced/index.php
+++ themes/jjpadvanced/index.php
@@ -44,6 +44,20 @@
                         <a href="<?php echo get_author_posts_url(get_the_author_meta('ID')); ?>">
                             <?php the_author(); ?>
                         </a>
+                        | Posted In
+                        <?php
+                            $categories = get_the_category();
+                            $separator = ", ";
+                            $output = '';
+
+                            if($categories){
+                                foreach($categories as $category){
+                                    $output .= '<a href="' . get_category_link($category->term_id) . '">'. $category->cat_name . '</a>'. $separator ;
+                                }
+                            }
+
+                            echo trim($output, $separator);
+                        ?>
                     </p>
                     <?php the_content(); ?>
                     <a href="#" class="button">Read More</a>
