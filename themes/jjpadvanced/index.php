458d1a0064e6da5618af39b6b57baa3fede56c81
jacekjp <jacekjp@gmail.com>
2018-12-03 16:33:19 +0100

    split index.php to use header and footer files

--- themes/jjpadvanced/index.php
+++ themes/jjpadvanced/index.php
@@ -1,34 +1,4 @@
-<!DOCTYPE html>
-<html <?php language_attributes(); ?>>
-<head>
-    <meta charset="<?php bloginfo('charset'); ?>">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?php bloginfo('name'); ?></title>
-    <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>"/>
-    <?php wp_head(); ?>
-</head>
-<body <?php body_class(); ?>>
-<header>
-    <div class="container">
-        <h1>
-            <a href="<?php echo home_url(); ?>"><?php bloginfo('name'); ?></a>
-            <small><?php bloginfo('description'); ?></small>
-        </h1>
-
-        <div class="h_right">
-            <form method="get" action="<?php esc_url(home_url('/')); ?>">
-                <input type="text" name="s" placeholder="Search..."/>
-            </form>
-        </div>
-    </div>
-</header>
-
-<nav class="nav main-nav">
-    <div class="container">
-        <?php $args = array('theme_location' => 'primary'); ?>
-        <?php wp_nav_menu($args); ?>
-    </div>
-</nav>
+<?php get_header(); ?>
 
 <div class="container content">
     <div class="main block">
@@ -82,23 +52,7 @@
         </div>
     </div>
 </div>
-<footer class="main-footer">
-    <div class="container">
-        <div class="f_left">
-            <p>&copy; 2018 - JJP</p>
-        </div>
-        <div class="f_right">
-            <ul>
-                <li><a href="index.html">Home</a></li>
-                <li><a href="about.html">About</a></li>
-                <li><a href="#">Services</a></li>
-            </ul>
-        </div>
-    </div>
-</footer>
-<?php wp_footer(); ?>
-</body>
-</html>
 
+<?php get_footer();?>
 
 
