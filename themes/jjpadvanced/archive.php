8f86a784086a4699af104da60036728dbd90155c
jacekjp <jacekjp@gmail.com>
2018-12-04 15:08:17 +0100

    add archive title

--- themes/jjpadvanced/archive.php
+++ themes/jjpadvanced/archive.php
@@ -2,12 +2,34 @@
 
 <div class="container content">
     <div class="main block">
+        <h1 class="page-header">
+            <?php
+            if (is_category()) {
+                single_cat_title();
+            } else if (is_author()) {
+                the_post();
+                echo 'Archives By Author: ' . get_the_author();
+                rewind_posts();
+            } else if (is_tag()) {
+                single_tag_title();
+            } else if (is_day()) {
+                echo 'Archives By Day: ' . get_the_date();
+            } else if (is_month()) {
+                echo 'Archives By Day: ' . get_the_date('F Y');
+            } else if (is_year()) {
+                echo 'Archives By Day: ' . get_the_date('Y');
+            } else {
+                echo 'Archives';
+            }
+            ?>
+        </h1>
         <?php if (have_posts()) : ?>
             <?php while (have_posts()) : the_post(); ?>
                 <div class="archive-post">
-                  <h4>
-                      <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
-                  </h4>
+                    <h4>
+                        <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+                    </h4>
+
                     <p>Posted On: <?php the_time('F j, Y g:i a'); ?></p>
                 </div>
             <?php endwhile; ?>
@@ -26,6 +48,6 @@
     </div>
 </div>
 
-<?php get_footer();?>
+<?php get_footer(); ?>
 
 
