f11f3fea279222236b6753794228037d20570bb2
jacekjp <jacekjp@gmail.com>
2018-12-04 16:29:35 +0100

    show page content

---
+++ themes/jjpadvanced/page.php
@@ -0,0 +1,29 @@
+<?php get_header(); ?>
+
+<div class="container content">
+    <div class="main block">
+        <?php if (have_posts()) : ?>
+            <?php while (have_posts()) : the_post(); ?>
+                <article class="page">
+                    <h2><?php the_title(); ?></h2>
+                    <?php the_content(); ?>
+                </article>
+            <?php endwhile; ?>
+        <?php else : ?>
+            <?php echo apautop('Soory, no posts ware found'); ?>
+        <?php endif; ?>
+    </div>
+    <div class="side">
+        <div class="block">
+            <h3>Sidebar Head</h3>
+
+            <p>Etizzle fo shizzle funky fresh sed augue hendrerit accumsizzle. I'm in the shizzle izzle fo shizzle.
+                Vivamizzle rizzle dolizzle, viverra vitae, sizzle izzle, ultrices izzle, sizzle.</p>
+            <a href="" class="button">More</a>
+        </div>
+    </div>
+</div>
+
+<?php get_footer();?>
+
+
