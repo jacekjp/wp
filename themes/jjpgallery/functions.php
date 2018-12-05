76776bccac7c287ca2d4f026619141bb90731e39
jacekjp <jacekjp@gmail.com>
2018-12-05 18:12:53 +0100

    add gallery post format support

--- themes/jjpgallery/functions.php
+++ themes/jjpgallery/functions.php
@@ -7,6 +7,8 @@ function jjp_gallery_theme_support(){
 
     set_post_thumbnail_size(900, 600);
 
+    //Post Format Support
+    add_theme_support('post-formats', array('gallery'));
 }
 
 add_action('after_setup_theme', 'jjp_gallery_theme_support');
