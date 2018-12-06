81b3cfe9d10f42b959cb8042ded6c1e5d049d988
jacekjp <jacekjp@gmail.com>
2018-12-06 15:52:12 +0100

    initialize sidebar widgets

--- themes/jjpbootstrap/functions.php
+++ themes/jjpbootstrap/functions.php
@@ -13,3 +13,17 @@ function theme_setup(){
 }
 
 add_action('after_setup_theme', 'theme_setup');
+
+//Widget Locations
+function init_widgets($id){
+    register_sidebar(array(
+        'name' => 'Sidebar',
+        'id' => 'sidebar',
+        'before_widget' => '<div class="panel panel-default">',
+        'after_widget' => '</div></div>',
+        'before_title' => '<div class="panel-heading"><h3 class="panel-title">',
+        'after_title' => '</h3></div><div class="panel-body">'
+    ));
+}
+
+add_action('widgets_init', 'init_widgets');
