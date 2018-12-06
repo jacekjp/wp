7ae139cb789352e1aec3412863d5064bf51dbe2c
jacekjp <jacekjp@gmail.com>
2018-12-06 15:44:57 +0100

    use navwalker menu

--- themes/jjpbootstrap/functions.php
+++ themes/jjpbootstrap/functions.php
@@ -1,8 +1,15 @@
 <?php
 
+require_once('wp_bootstrap_navwalker.php');
+
 function theme_setup(){
     //Featured Image Support
     add_theme_support('post-thumbnails');
+
+    //Nav Menus
+    register_nav_menus(array(
+        'primary' => __('Primary Menu')
+    ));
 }
 
 add_action('after_setup_theme', 'theme_setup');
